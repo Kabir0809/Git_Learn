ae6ad63cb0261ca76cc4ed907fec4c7f87946b5c
Aummehta <aum281004@gmail.com>
2024-07-09 10:46:19 +0530

    Aum-Changes

---
+++ if.py
@@ -0,0 +1,14 @@
+# c=int(input("Enter the number : "))
+# if c%2==0:
+#     print(f"{c} is even")
+# else:
+#     print(f"{c} is odd")
+
+indian=["samosa","khaman"]
+chinese=["manchurian","noodles"]
+
+dish=input("Enter the dish name : ")
+if dish in indian:
+    print("indian")
+elif dish in chinese:
+    print("chinese")
